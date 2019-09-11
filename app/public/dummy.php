8279f331874ab55cccb2500a485e3dd608ab17ab
nhemmer <54485148+nhemmer@users.noreply.github.com>
2019-09-10 23:15:27 -0400

    updating according to Kaltura video

---
+++ app/public/dummy.php
@@ -0,0 +1,28 @@
+{
+  "patientGuid": "1038",
+  "firstName": "Jake",
+  "lastName": "Gyllenhall",
+  "dob": "1981-07-18",
+  "sexAtBirth": "M",
+},
+{
+  "patientGuid": "1046",
+  "firstName": "Meryl",
+  "lastName": "Streep",
+  "dob": "1950-12-15",
+  "sexAtBirth": "F",
+},
+{
+  "patientGuid": "1759",
+  "firstName": "Tom",
+  "lastName": "Hardy",
+  "dob": "1979-08-13",
+  "sexAtBirth": "M",
+},
+{
+  "patientGuid": "1335",
+  "firstName": "Nicole",
+  "lastName": "Kidman",
+  "dob": "1969-11-14",
+  "sexAtBirth": "F",
+}
