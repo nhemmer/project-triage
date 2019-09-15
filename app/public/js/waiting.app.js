fb5c308aef4c25372662f1aa091188bd0c9d52d4
nhemmer <54485148+nhemmer@users.noreply.github.com>
2019-09-15 19:40:08 -0400

    fixing code from kaltura video

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -7,17 +7,48 @@ var waitingApp = new Vue({
         last: ''
       },
     },
-    patients: []
+    patients: [
+      {
+        "patientGuid": "1038",
+        "firstName": "Jake",
+        "lastName": "Gyllenhall",
+        "dob": "1981-07-18",
+        "sexAtBirth": "M",
+        "priority": "high"
+      },
+      {
+        "patientGuid": "1046",
+        "firstName": "Meryl",
+        "lastName": "Streep",
+        "dob": "1950-12-15",
+        "sexAtBirth": "F",
+        "priority": "medium"
+      },
+      {
+        "patientGuid": "1759",
+        "firstName": "Tom",
+        "lastName": "Hardy",
+        "dob": "1979-08-13",
+        "sexAtBirth": "M",
+        "priority": "low"
+      },
+      {
+        "patientGuid": "1335",
+        "firstName": "Nicole",
+        "lastName": "Kidman",
+        "dob": "1969-11-14",
+        "sexAtBirth": "F",
+        "priority": "high"
+      }
+    ]
   },
 methods: {
   fetchPatients() {
     fetch('dummy.php')
-    .then(response => response.json() )
-    .then( json => {waitingApp.patients = json})
-    ;
+    .then(function(response){waitingApp.patients = response.json()};);
   }
 },
-  created() {
-    this.fetchPatients();
+created() {
+  this.fetchPatients()
 }
 });
